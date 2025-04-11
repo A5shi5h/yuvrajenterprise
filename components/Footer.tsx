05b86dbada8061a71327641ee7df34bb89843726
Ashish Lamichaney <124287828+A5shi5h@users.noreply.github.com>
2025-04-11 22:26:11 +0530

    basic debugging

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { motion } from "framer-motion";
-import { Facebook, Instagram, Linkedin, Youtube } from "lucide-react";
 import Image from "next/image";
 
 const Footer = () => {
